1cc7c5df5f6b15d5882a1c749c4e4f9603fb4df4
metalmama <sofie@hancocksit.com>
2022-09-28 08:56:08 +0200

    updating all files

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,33 +1,37 @@
+
+from pydoc import doc
+from time import sleep
+from typing import List, Dict
 import json
 from flask import Flask,render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
  
-app.config['MYSQL_HOST'] = 'mysql' #use 127.0.0.1 when not in another container
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Passw0rd!'
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'peoples'
-#app.config['MYSQL_PORT'] = 3338 - only used when connecting on host
 
 mysql = MySQL(app)  
+ 
+#Creating a connection cursor
+cursor = mysql.connection.cursor()
 
 def peeps():
-    cursor = mysql.connection.cursor()
-    cursor.execute('SELECT name, age FROM peeps')
+    cursor.execute('SELECT * FROM peeps')
     results = [{name: age} for (name, age) in cursor]
 
     return json.dumps(results)
 
 def insertperson(data):
     try:
-        cursor = mysql.connection.cursor()
-        cursor.execute("INSERT INTO peeps (name,age) VALUES (%s,%s);",(data['name'], data['age']))
-        mysql.connection.commit()
-        return 'INSERT OK'
+        cursor.execute("INSERT INTO peeps (name,age) VALUES (%s,%s)",(data['name'], data['age']))
+        mysql.commit()
+        return True
     except Exception as e:
-        return "Problem inserting into db: " + str(e)
-        
+        print("Problem inserting into db: " + str(e))
+        return False
 
 @app.route('/persons', methods = ['GET'])
 def index():
@@ -35,7 +39,10 @@ def index():
 
 @app.route('/persons', methods = ['POST'])
 def insert():
-    return insertperson(request.json)
+    if insertperson(request.json):
+        return "Insert OK"
+    else:
+        return "Insert FAILED"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
